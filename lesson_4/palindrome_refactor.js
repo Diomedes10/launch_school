4206afa6452c9c2434efd1dcc7a8292c765037d8
Diomedes10 <dane.lund@lundventuresllc.com>
2021-06-11 10:20:57 -0500

    palindrome refactor

---
+++ lesson_4/palindrome_refactor.js
@@ -0,0 +1,41 @@
+function substrings(str) {
+  let result = [];
+  let startingIndex = 0;
+
+  while (startingIndex <= str.length - 2) {
+    let numChars = 2;
+    while (numChars <= str.length - startingIndex) {
+      let substring = str.slice(startingIndex, startingIndex + numChars);
+      result.push(substring);
+      numChars += 1;
+    }
+
+    startingIndex += 1;
+  }
+
+  return result;
+}
+
+function isPalindrome(str) {
+  return str === str.split('').reverse().join('');
+}
+
+
+function palindromeSubstrings(str) {
+  let result = [];
+  let substringArr = substrings(str);
+
+  substringArr.forEach(substring => {
+    if (isPalindrome(substring)) {
+      result.push(substring);
+    }
+  });
+
+  return result;
+
+}
+
+console.log(palindromeSubstrings("supercalifragilisticexpialidocious"));
+console.log(palindromeSubstrings("abcddcbA"));
+console.log(palindromeSubstrings("palindrome"));
+console.log(palindromeSubstrings(""));
